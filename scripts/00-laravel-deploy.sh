c16161cf0e66e4ee94271ab2586c9b0c4d7a9894
y0114 <212836122+y0144@users.noreply.github.com>
2025-06-09 14:11:24 +0900

    deploytest 10

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Running composer"
 composer config -g repos.packagist composer
-composer --no-dev --working-dir=/var/www/html
+composer install --no-dev --working-dir=/var/www/html
 
 echo "Running migrations..."
 php artisan migrate --force
